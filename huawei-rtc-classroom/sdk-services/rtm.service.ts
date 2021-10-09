c2161942b677d9493beee2f16c35cb06befb2f70
mad-hu <450287549@qq.com>
2021-10-09 16:39:33 +0800

    update: rtm service instance

--- huawei-rtc-classroom/sdk-services/rtm.service.ts
+++ huawei-rtc-classroom/sdk-services/rtm.service.ts
@@ -2,15 +2,14 @@
  * @Author: Yandong Hu
  * @github: https://github.com/Mad-hu
  * @Date: 2021-08-05 11:03:33
- * @LastEditTime: 2021-09-30 15:43:01
+ * @LastEditTime: 2021-10-09 16:18:13
  * @LastEditors: Yandong Hu
  * @Description: RTM 基础调用服务
  */
-import { RTMBaseProvider } from "./abstract/rtm.abstract";
 import AgoraRTMSdkWebService from "./agora/sdk/rtm/agora-rtm-sdk-web.service";
 import { sdk_build_config } from "./build";
 import WangyiRTMSdkWebService from "./wangyi/sdk/rtm/wangyi-rtm-sdk-web.service";
-let rtmInstance: RTMBaseProvider;
+let rtmInstance: AgoraRTMSdkWebService | WangyiRTMSdkWebService;
 const getBuildRtmCompany = () => {
   return sdk_build_config.rtm.company;
 }
--- huawei-rtc-classroom/vue/src/services/common/rtm.service.ts
+++ huawei-rtc-classroom/vue/src/services/common/rtm.service.ts
@@ -2,33 +2,26 @@
  * @Author: Yandong Hu
  * @github: https://github.com/Mad-hu
  * @Date: 2021-08-05 11:03:33
- * @LastEditTime: 2021-08-10 13:38:25
+ * @LastEditTime: 2021-10-09 16:39:25
  * @LastEditors: Yandong Hu
  * @Description: RTM 基础调用服务
  */
-import { RTMBaseProvider } from "./abstract/rtm.abstract";
 import AgoraRTMSdkWebService from "./agora/sdk/rtm/agora-rtm-sdk-web.service";
 import { sdk_build_config } from "./build";
 import WangyiRTMSdkWebService from "./wangyi/sdk/rtm/wangyi-rtm-sdk-web.service";
-
-let rtmTmp: RTMBaseProvider;
-let rtmInstance: RTMBaseProvider;
-
-
+let rtmInstance: AgoraRTMSdkWebService | WangyiRTMSdkWebService;
 export const getBuildRtmCompany = () => {
   return sdk_build_config.rtm.company;
 }
 
-
 export const RtmService = () => {
   if (rtmInstance) return rtmInstance;
   if (sdk_build_config.rtm.company == 'wangyi') {
-    rtmTmp = new WangyiRTMSdkWebService();
+    rtmInstance = new WangyiRTMSdkWebService();
   }
 
   if (sdk_build_config.rtm.company == 'agora') {
-    rtmTmp = new AgoraRTMSdkWebService();
+    rtmInstance = new AgoraRTMSdkWebService();
   }
-  rtmInstance = rtmTmp;
   return rtmInstance;
 };
