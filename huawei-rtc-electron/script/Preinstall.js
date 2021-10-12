db61772137883502e4dd1ad547c391908e107f57
mad-hu <450287549@qq.com>
2021-10-12 17:08:47 +0800

    fix # electron project yarn reinstall error

--- huawei-rtc-electron/script/Preinstall.js
+++ huawei-rtc-electron/script/Preinstall.js
@@ -2,7 +2,7 @@
  * @Author: Yandong Hu
  * @github: https://github.com/Mad-hu
  * @Date: 2021-09-13 09:02:45
- * @LastEditTime: 2021-09-13 11:59:00
+ * @LastEditTime: 2021-10-12 17:06:52
  * @LastEditors: Yandong Hu
  * @Description:
  */
@@ -11,6 +11,12 @@ const fse = require('fs-extra')
 
 const srcPath = path.join(__dirname, `../sdk/${process.platform == "win32" ? 'windows': 'mac'}/`);
 const destPath = path.join(__dirname, `../sdk/`);
+
+console.log('check hrtc sdk.');
+if(fse.existsSync(path.join(destPath, 'hrtc-electron-sdk.tar.gz'))) {
+  console.log('sdk exist!');
+  return;
+}
 fse.copy(srcPath, destPath, function (err) {
   if (err) {
     console.log("copy dir error:" + err);
